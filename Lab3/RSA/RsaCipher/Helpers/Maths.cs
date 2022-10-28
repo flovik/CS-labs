c646e347883c5a1bd9d0c5a813ca596bcfabbec7
Victor Florescu <victor.florescu@amdaris.com>
2022-10-28 16:28:20 +0300

    finished lab 3 - rsa cipher

---
+++ Lab3/RSA/RsaCipher/Helpers/Maths.cs
@@ -0,0 +1,77 @@
+﻿using System.Numerics;
+using System.Reflection;
+
+namespace RSA.RsaCipher.Helpers;
+
+public static class Maths
+{
+    public static BigInteger LeastCommonMultiple(BigInteger P, BigInteger Q)
+    {
+        //apply euclidean algorithm to find lcm
+        var ab = P * Q;
+        var gcd = Gcd(P, Q);
+        return ab / gcd;
+    }
+
+    private static BigInteger Gcd(BigInteger a, BigInteger b)
+    {
+        //at each iteration, a or b will hold the remainder of mod operation 
+        //other one will hold its predecessor and vice versa until remainder is 0 
+        while (a != 0 && b != 0)
+        {
+            if (a > b)
+                a %= b;
+            else
+                b %= a;
+        }
+
+        return a | b;
+    }
+
+    public static BigInteger ModularInverse(BigInteger left, BigInteger mod)
+    {
+        //returns a and b factors of ax + by = gcd(left, mod), only need a
+        var egcd = ExtendedGcd(left, mod);
+
+        var result = egcd.a;
+
+        if (result < 0)
+            result += mod;
+
+        return result % mod;
+    }
+
+    private static (BigInteger a, BigInteger b) 
+        ExtendedGcd(BigInteger inverseOf, BigInteger mod)
+    {
+        //function returns both s and t, can be simplified to return only one value
+
+        //sa + tb = gcd(a,b)
+        //inverseOf (calculate inverse of input), mod (with corresponding modulo) 
+        //q (quotient of mod / inverseOf), r (remainder of mod / inverseOf)
+        
+        //first row initial values
+        BigInteger s1 = 0;
+        BigInteger t1 = 1;
+        BigInteger s2 = 1;
+        BigInteger t2 = 0;
+
+        while (inverseOf != 0)
+        {
+            var q = mod / inverseOf;
+            var r = mod % inverseOf;
+
+            var s3 = s1 - s2 * q;
+            var t3 = t1 - t2 * q;
+
+            mod = inverseOf; //as in euclidean algorithm, move the previous inverseOf to current mod
+            inverseOf = r; //inverseOf will be the remainder of 
+            s1 = s2; //s1 becomes s2 from previous row
+            t1 = t2; //t1 becomes t2 from previous row 
+            s2 = s3; //s2 becomes s3 from previous row
+            t2 = t3; //t2 becomes t3 from previous row 
+        }
+
+        return (s1, t1);
+    }
+}
---
+++ Lab3/RSA/RsaCipher/RsaCipher.cs
@@ -0,0 +1,57 @@
+﻿using System.Numerics;
+using System.Text;
+using RSA.RsaCipher.Helpers;
+using RSA.RsaCipher.Interfaces;
+using static RSA.RsaCipher.Helpers.Utils;
+namespace RSA.RsaCipher;
+
+public class RsaCipher : IRsaCipher
+{
+    private readonly (BigInteger N, BigInteger D) PrivateKey;
+    private readonly (BigInteger N, BigInteger E) PublicKey;
+    private readonly IKeyGenerator KeyGenerator;
+
+    public RsaCipher()
+    {
+        KeyGenerator = new KeyGenerator();
+        PrivateKey = KeyGenerator.GeneratePrivateKey();
+        PublicKey = KeyGenerator.GeneratePublicKey();
+    }
+
+    public List<string> Encrypt(string plaintext)
+    {
+        //get ascii representation for each character in plaintext
+        var plainTextInts = StringToInts(plaintext);
+        //encrypt with public key
+        var encryptedInts = EncryptDecryptMessage(plainTextInts, (int) PublicKey.E);
+        //return hex encoded result list
+        return IntToHexList(encryptedInts);
+    }
+
+    public string Decrypt(List<string> ciphertext)
+    {
+        //convert hex values to int
+        var encryptedInts = HexToInts(ciphertext);
+        //decrypt with private key
+        var decryptedInts = EncryptDecryptMessage(encryptedInts, (int) PrivateKey.D);
+        //transform ascii ints to string
+        return IntsToString(decryptedInts);
+    }
+
+    private int[] EncryptDecryptMessage(int[] plaintextBytes, int key)
+    {
+        var result = new int[plaintextBytes.Length];
+
+        for (int i = 0; i < plaintextBytes.Length; i++)
+        {
+            //c = m ^ e (mod n), m = plaintext, e = public key/private key
+            //raise m to the power of key (public/private)
+            var encrypted = BigInteger.Pow(plaintextBytes[i], key);
+            //mod n
+            encrypted %= PublicKey.N;
+            result[i] = (int) encrypted;
+        }
+
+        return result;
+    }
+}
